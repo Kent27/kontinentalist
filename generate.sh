617f505a8d45cbb2d5f53fc1d08fe401ae89f9b8
Alvin Theodora <alvintheodoraa@gmail.com>
2019-07-16 20:56:04 +0700

    using local npm in script, set default client id 2 in env

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 cp .env.example .env
 docker-compose up --build -d
 docker-compose exec app composer install
-docker-compose exec app npm install
+npm install
 docker-compose exec app php artisan key:generate
 docker-compose restart
 docker-compose exec app php artisan migrate:fresh --seed
